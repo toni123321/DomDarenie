c34ecf37e5230639a2f5ddb91c46e57e21c8f2b7
Antonio Takev <tonitakev@gmail.com>
2019-03-16 09:43:52 +0200

    routing changes

--- project/app.py
+++ project/app.py
@@ -3,6 +3,10 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
+def index():
+	return render_template('home.html')
+
+@app.route('/home')
 def home():
 	return render_template('home.html')
 	
@@ -18,6 +22,10 @@ def login():
 def register():
 	return render_template('register.html')
 
+@app.route('/about')
+def about():
+	return render_template('about.html')
+	
 @app.route('/donate')
 def donate():
 	return render_template('donate.html')
